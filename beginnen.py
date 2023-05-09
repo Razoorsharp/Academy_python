97ecb30b9e2ddde4b689232d34f16a28d140e8d1
Razoorsharp <54915084+Razoorsharp@users.noreply.github.com>
2023-05-09 16:01:24 +0200

    after first class
    .

--- beginnen.py
+++ beginnen.py
@@ -1,1 +1,16 @@
 print ("ja ik ben begonnen met python")
+
+#NOTES
+# integer int
+# floating point is float\
+#     strings is str
+#     dictionaries, key value zelfde als een array  voorbeeld  {"mykey": "value, "name":"test"}
+#     tupes tup (10,"helloo",200.3) ordered immutable sequence of objects. \
+#     Sets .. set unordered collection of unique objects {"a", "b"}
+#     boolean bool true false. 
+pim = {"voornaam": "pim", "achternaam":"damen"}
+
+len(pim) # check lengte dus geen count()
+
+print("hello /nWorld") # nieuwe regel
+print("hello tworld") # /t is een tab er tussen. 
